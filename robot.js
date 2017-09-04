64ed6e7d4e79c49482bff78d137e8bf2fcdfd29f
Lorenzo Romagnoli <lorenzormgnl@gmail.com>
2017-09-04 19:48:44 +0200

    added http modules for controlling the robot from web ui

--- robot.js
+++ robot.js
@@ -1,5 +1,7 @@
 var Cylon = require('cylon');
 
+Cylon.api('http');
+
 Cylon.robot({
   connections: {
     audio: { adaptor: 'audio' }
@@ -10,11 +12,27 @@ Cylon.robot({
   },
 
   work: function(my) {
-    my.audio.on("complete", function(){
-      console.log("Done playing this nice sound.");
-    });
+    // my.audio.on("complete", function(){
+    //   console.log("Done playing this nice sound.");
+    // });
 
     // You can pass a string with a full or relative path here,
-    my.audio.play('./assets/sound/meow.mp3');
-  }
+    every((5).seconds(), function(){
+
+      console.log("Hello world!");
+      my.audio.play('./assets/sound/meow.mp3');
+
+    });
+
+  },
+
+  commands: {
+     do_a_thing: function() { this.doAThing.call(this); }
+   },
+
+  doAThing: function() {
+   console.log("I did a thing!");
+ }
+
+
 }).start();
