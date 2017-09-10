8668a33203b45518a0ad156cb1e4841c2a05eb56
Lorenzo Romagnoli <lorenzormgnl@gmail.com>
2017-09-10 10:38:17 +0200

    automatically choses serial port

--- README.md
+++ README.md
@@ -5,5 +5,14 @@ The second version of shybo.
 ### hardware
   based on raspi zero w + arduino communicating over serialport
 
-### dependencies
-  
+### installation
+
+#####upgrade node js and npm
+
+'
+$ wget -O - https://raw.githubusercontent.com/sdesalas/node-pi-zero/master/install-node-v7.7.1.sh | bash
+'
+
+##### install dependencies
+
+npm install
--- drivers/cylon-myArduino/lib/adaptor.js
+++ drivers/cylon-myArduino/lib/adaptor.js
@@ -8,15 +8,41 @@ var SerialPort = require('serialport');
 var Adaptor = module.exports = function Adaptor(opts) {
   Adaptor.__super__.constructor.apply(this, arguments);
   opts = opts || {};
+
 };
 
 Cylon.Utils.subclass(Adaptor, Cylon.Adaptor);
 
 Adaptor.prototype.connect = function(callback) {
-  callback();
+  this.SerialPortName = "";
 
-  this.connector = this.myArduino = this.port = new SerialPort('/dev/cu.usbmodem1421', {
-    baudRate: 9600
+  SerialPort.list( (err, ports) =>  {
+
+    ports.forEach(  (port)=> {
+      //if on mac
+      if (port.comName.indexOf('/dev/tty.usbmodem') > -1) {
+        this.SerialPortName = port.comName;
+      }
+      //if on raspi
+      else if (port.comName.indexOf('/dev/ttyACM') > -1) {
+        this.SerialPortName = port.comName;
+      }
+    });
+
+    if (this.SerialPortName != "") {
+      this.connector = this.myArduino = this.port = new SerialPort(this.SerialPortName, {
+        baudRate: 9600
+      }, function(err) {
+        if (err) {
+          Cylon.Logger.log("connection to serial port didn't work");
+        } else {
+          Cylon.Logger.log("connected to " + this.SerialPortName);
+          callback();
+        }
+      });
+    } else {
+      Cylon.Logger.log("are you sure you connected your arduino???");
+    }
   });
 
 };
@@ -25,8 +51,8 @@ Adaptor.prototype.disconnect = function(callback) {
   callback();
 };
 
-Adaptor.prototype.digitalWrite=function(pin, value){
-  var message='DW/'+pin+'/'+value+'\r'+'\n';
-  this.myArduino.write (message);
-  Cylon.Logger.log("written "+ message + " to myArduino");
+Adaptor.prototype.digitalWrite = function(pin, value) {
+  var message = 'DW/' + pin + '/' + value + '\r' + '\n';
+  this.myArduino.write(message);
+  Cylon.Logger.log("written " + message + " to myArduino");
 }
