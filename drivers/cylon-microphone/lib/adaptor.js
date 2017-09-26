0050dc21203bad2fd454c6ce24e622f95caa9a1e
Lorenzo Romagnoli <lorenzormgnl@gmail.com>
2017-09-26 17:32:04 +0200

    enabling mic debug

--- drivers/cylon-microphone/lib/adaptor.js
+++ drivers/cylon-microphone/lib/adaptor.js
@@ -15,7 +15,7 @@ var Adaptor = module.exports = function Adaptor(opts) {
 	this.connector = this.microphone = mic({
 		rate: '16000',
 		channels: '1',
-		debug: false,
+		debug: true,
 		exitOnSilence: 6,
 		buffer:500,
 	});
