ef2f5074c532b30282ef6337505672c9aac1af8c
Lorenzo Romagnoli <lorenzormgnl@gmail.com>
2017-10-14 10:11:00 +0200

    removes one command from the parallel list

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #sleep 8
 
-# #create a virtual monitor if on raspi
-# sudo Xvfb :1 -screen 0 1024x768x24 </dev/null &
-# export DISPLAY=":1"
-#
+#create a virtual monitor if on raspi
+sudo Xvfb :1 -screen 0 1024x768x24 </dev/null &
+export DISPLAY=":1"
+
 # #open wekinator
 # java -jar ./utils/wekinator/WekiMini.jar ./assets/wek/test/WekinatorProject/WekinatorProject.wekproj &
 #
@@ -18,5 +18,4 @@
 # node ./robot.js &
 
 
-. utils/parallel_commands.sh parallel_commands "sudo Xvfb :1 -screen 0 1024x768x24 </dev/null &
-export DISPLAY=":1"" "java -jar ./utils/wekinator/WekiMini.jar ./assets/wek/test/WekinatorProject/WekinatorProject.wekproj" "python utils/osc_audio_djImpostore.py" "node ./robot.js"
+. utils/parallel_commands.sh parallel_commands "java -jar ./utils/wekinator/WekiMini.jar ./assets/wek/test/WekinatorProject/WekinatorProject.wekproj" "python utils/osc_audio_djImpostore.py" "node ./robot.js"
