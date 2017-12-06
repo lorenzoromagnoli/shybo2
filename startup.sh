8c2ceada94aa5e9d23d86a763942eeb7bbbd0fb3
Lorenzo Romagnoli <lorenzormgnl@gmail.com>
2017-12-06 21:46:20 +0100

    testing new delay in init script

--- startup.sh
+++ startup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#sleep 8
+sleep 8
 
 #create a virtual monitor if on raspi
 sudo Xvfb :1 -screen 0 1024x768x24 </dev/null &
 export DISPLAY=":1"
+
+sleep 2
 
 # #open wekinator
 # java -jar ./utils/wekinator/WekiMini.jar ./assets/wek/test/WekinatorProject/WekinatorProject.wekproj &
